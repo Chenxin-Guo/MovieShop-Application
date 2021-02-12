146cde04f57c19e22e9671ad86939b5dc08fbb8b
Chenxin-Guo <guochenxin_2017@163.com>
2021-02-12 17:27:54 -0500

    init movie shop project

---
+++ MovieShop.MVC/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // https://localhost:44363/Movies/Index, we usually delete controller from the class name
+        // https://localhost:44363/movies/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details()
+        {
+            //by default when u have return view(), but you can change that.
+            return View();
+            //return View("Testing");
+        }
+        [HttpGet]
+        public IActionResult TopRevenueMovies()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopRatedMovies()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Genre(int genreId)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Reviews(int movieId)
+        {
+            return View();
+        }
+
+    }
+}
